c22e664e96282f21b7e1737369afb336886f1a84
alexlara-mon <alex.lara@monato.com>
2026-06-09 12:18:51 -0600

    fix(1xbet): update CLABE and beneficiary to Fincopay

--- src/1xbet/components/MonatoPayModal.tsx
+++ src/1xbet/components/MonatoPayModal.tsx
@@ -188,9 +188,9 @@ export default function MonatoPayModal({ open, onClose, defaultTab }: Props) {
                     <div className="monato-result__sub">Transfiere ${speiReceive} MXN desde tu banco</div>
                   </div>
                   <div className="monato-info-card">
-                    <div className="monato-info-card__row"><span className="monato-info-card__label">CLABE</span><span className="monato-info-card__value">012180001234567890</span></div>
-                    <div className="monato-info-card__row"><span className="monato-info-card__label">Beneficiario</span><span className="monato-info-card__value">Monato Pay México</span></div>
-                    <div className="monato-info-card__row"><span className="monato-info-card__label">Banco</span><span className="monato-info-card__value">STP</span></div>
+                    <div className="monato-info-card__row"><span className="monato-info-card__label">CLABE</span><span className="monato-info-card__value">734180999000000006</span></div>
+                    <div className="monato-info-card__row"><span className="monato-info-card__label">Beneficiario</span><span className="monato-info-card__value">Fincopay</span></div>
+                    <div className="monato-info-card__row"><span className="monato-info-card__label">Banco</span><span className="monato-info-card__value">Fincopay</span></div>
                     <div className="monato-info-card__row"><span className="monato-info-card__label">Concepto</span><span className="monato-info-card__value">1XBET-1688885881</span></div>
                     <div className="monato-info-card__row"><span className="monato-info-card__label">Importe</span><span className="monato-info-card__value">${speiReceive} MXN</span></div>
                   </div>
